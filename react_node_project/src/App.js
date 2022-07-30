3f48cb0121ca9f552d46595d38669210458b6018
krishnarchana <gopikrishna.e84@gmail.com>
2022-07-30 15:30:07 +0530

    page 2 and page 3 alignment issues fixed, done code formatting, removed all inline css and moved to app.css file

--- react_node_project/src/App.js
+++ react_node_project/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import { useEffect, useState } from "react";
 import accordianData from './data/accordianData';
 
 function App() {
@@ -18,37 +17,37 @@ function App() {
         }
     };
 
-  return (
-    <div className="App">
-        <header className="App-header">       
-            <p>PTO Preference Portal</p>
-        </header>
-        <form action="/guideLinesOne" className="loginForm">
-            <label className="formLabel" htmlFor="userLogin">User Login</label>
-            <br/>
-            <input type="text" id="userLoginId" name="userLogin" placeholder="LANID" />
-            <br/>
-            <span>Unauthorised Access is prohibited</span>
-            <br/>
-            <input type="submit" value="Login" />
-        </form>
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>PTO Preference Portal</p>
+            </header>
+            <form action="/guideLinesOne" className="loginForm">
+                <label className="formLabel" htmlFor="userLogin">User Login</label>
+                <br />
+                <input type="text" id="userLoginId" name="userLogin" placeholder="LANID" />
+                <br />
+                <span>Unauthorised Access is prohibited</span>
+                <br />
+                <input type="submit" value="Login" />
+            </form>
 
-        <p className="formParagraph">Important Round and Phase Guidelines</p>
-        <hr className="formHr" />
+            <p className="formParagraph">Important Round and Phase Guidelines</p>
+            <hr className="formHr" />
 
-          {accData.map((item, index) => {
-              return (
-                  <div key={index}>
-                      <button className="accordion" onClick={(e)=>accordianClick(index)}>{item.title}</button>
-                      <div className="panel">
-                        <p>{item.content}</p>
-                      </div>
-                  </div>
-                  )
-          })}
-          
-    </div>
-  );
+            {accData.map((item, index) => {
+                return (
+                    <div key={index}>
+                        <button className="accordion" onClick={(e) => accordianClick(index)}>{item.title}</button>
+                        <div className="panel">
+                            <p>{item.content}</p>
+                        </div>
+                    </div>
+                )
+            })}
+
+        </div>
+    );
 }
 
 export default App;
--- react_node_project/src/GuidelinesOne.js
+++ react_node_project/src/GuidelinesOne.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 function GuidelinesOne() {
     const navigate = useNavigate();
     const [guideLinesStateFlag, setGuideLinesStateFlag] = useState(true);
     const [videoFlag, setVideoFlag] = useState(false);
+
     const changePageTwo = (e) => {
         setGuideLinesStateFlag(false);
     }
@@ -20,82 +21,87 @@ function GuidelinesOne() {
         <div className="App">
             <header className="App-header">
                 <div className="titleDiv">
-                <p className="fullscreenTitle">Hello @Username, Welcome to your Admin PTO Preference Portal</p>
-                <span className="mobilescreenTitle">Welcome to your Admin PTO Portal</span>
-                <div className="logoutClass">
-                        <i className="gg-log-out" onClick={logoutHandler}></i>
-                </div>
+                    <div className="leftHeaderCss">
+                        <span className="fullscreenTitle">Hello @Username, Welcome to your Admin PTO Preference Portal</span>
+                        <span className="mobilescreenTitle">Welcome to your Admin PTO Portal</span>
+                    </div>
+                    <div className="rightHeaderCss">
+                        <div className="logoutClass">
+                            <i className="gg-log-out" onClick={logoutHandler}></i>
+                        </div>
+                    </div>
                 </div>
             </header>
             <p className="usernameTitle">Hello @Username</p>
             <div id="outer">
                 <div className="leftBar">
-                  <p>  PTO Preference Portal Tutorial </p>
+                    <p>  PTO Preference Portal Tutorial </p>
                 </div>
                 <div className="rightBar">
-                    <div className="guideLinesText">                        
+                    <div className="guideLinesText">
                         {guideLinesStateFlag ? (<p className="guideLinesPOne">Important Round and Phase Guidelines</p>) : (<p className="guideLinesPOne">PTO Tutorial</p>)}
                         {guideLinesStateFlag ? (<p className="guideLinesPTwo">1 of 2</p>) : (<p className="guideLinesPTwo">2 of 2</p>)}
                     </div>
-                    <hr className="hrClass"/>
+                    <hr className="hrClass" />
                     {guideLinesStateFlag ? (<div> <div>
-                        <span style={{ float: 'left', fontWeight:'bold' }}>Round 1 - Phase 1</span>
-                        <br/>
-                        <ul>
-                            <li style={{textAlign:'left'} }>Full time employees: May select 40 consecutive hours (5 business days) of your avaiable PTO allowance. Please note: Part time employees may select 5 consecutive working days; however, cumulative hours may vary based on your scheduled shift.</li>
-                        </ul>
-                    </div>
-                    <div>
-                        <span style={{ float: 'left', fontWeight: 'bold' }}>Other Considerations</span>
+                        <span className="spanText">Round 1 - Phase 1</span>
                         <br />
                         <ul>
-                            <li style={{ textAlign: 'left' }}>Only full days are available in Round 1 - Phase 1. Waitlisted dates are not available.</li>
-                            <li style={{ textAlign: 'left' }}>Weekend dates and NM Recognized Holidays are not considered business days and should be excluded as part of the 40 consecutive hours (5 business days).</li>
-                            <li style={{ textAlign: 'left' }}>Blocked dates can be waitlisted; however, restricted dates are not available for waitlist.</li>
-                            <li style={{ textAlign: 'left' }}>There is no option to bypass any round or phase if desired.</li>
+                            <li className="liText">Full time employees: May select 40 consecutive hours (5 business days) of your avaiable PTO allowance. Please note: Part time employees may select 5 consecutive working days; however, cumulative hours may vary based on your scheduled shift.</li>
                         </ul>
                     </div>
-                    <div>
-                        <span style={{ float: 'left', fontWeight: 'bold' }}>Round 1 - Phase 2</span>
-                        <br />
-                        <ul>
-                            <li style={{ textAlign: 'left' }}>Employees may select up to 50% of your remaining PTO allowance minus any selected hours in Round 1 Phase 1.</li>
-                            <li style={{ textAlign: 'left' }}>Employees may select both consecutive and/or non-consecutive days.</li>
-                            <li style={{ textAlign: 'left' }}>Employees may select full days, half days, waitlist full days, and/or waitlist half days.</li>
-                            <li style={{ textAlign: 'left' }}>Weekend days and NM Recognized Holidays are excluded.</li>
-                        </ul>
-                    </div>
-                    <div style={{ marginBottom:'2rem' }}>
-                        <span style={{ float: 'left', fontWeight: 'bold' }}>Round 2</span>
-                        <br />
-                        <ul>
-                            <li style={{ textAlign: 'left' }}>Employees may select all of their remaining PTO allowance.</li>
-                        </ul>
-                    </div>
+                        <div>
+                            <span className="spanText">Other Considerations</span>
+                            <br />
+                            <ul>
+                                <li className="liText">Only full days are available in Round 1 - Phase 1. Waitlisted dates are not available.</li>
+                                <li className="liText">Weekend dates and NM Recognized Holidays are not considered business days and should be excluded as part of the 40 consecutive hours (5 business days).</li>
+                                <li className="liText">Blocked dates can be waitlisted; however, restricted dates are not available for waitlist.</li>
+                                <li className="liText">There is no option to bypass any round or phase if desired.</li>
+                            </ul>
                         </div>
+                        <div>
+                            <span className="spanText">Round 1 - Phase 2</span>
+                            <br />
+                            <ul>
+                                <li className="liText">Employees may select up to 50% of your remaining PTO allowance minus any selected hours in Round 1 Phase 1.</li>
+                                <li className="liText">Employees may select both consecutive and/or non-consecutive days.</li>
+                                <li className="liText">Employees may select full days, half days, waitlist full days, and/or waitlist half days.</li>
+                                <li className="liText">Weekend days and NM Recognized Holidays are excluded.</li>
+                            </ul>
+                        </div>
+                        <div style={{ marginBottom: '2rem' }}>
+                            <span className="spanText">Round 2</span>
+                            <br />
+                            <ul>
+                                <li className="liText">Employees may select all of their remaining PTO allowance.</li>
+                            </ul>
+                        </div>
+                    </div>
                     ) : <div>
-                            <div style={{
-                                width: '100%', height: "15rem", border: '1px solid lightgrey'
-                            }}>
-                                {!videoFlag ? <div style={{ marginTop: '6rem' }}>
-                                    <span>View Tutorial Video</span>
-                                    <br />
-                                    <br />
-                                    <span className="gg-play-button-r" onClick={playVideoHandler}></span>
-                                   
-                                </div>
-                                    : (<video controls>
-                                        <source src="./assets/sampleVideo.mp4" type="video/mp4" />
-                                        <source src="./assets/sampleVideo.ogg" type="video/ogg" />
-                                        Your browser does not support the video tag.
-                                    </video>) }
-                                </div>
+                        <div className="videoMainDiv">
+                            {!videoFlag ? <div style={{ marginTop: '6rem' }}>
+                                <span>View Tutorial Video</span>
+                                <br />
+                                <br />
+                                <span className="gg-play-button-r" onClick={playVideoHandler}></span>
+
+                            </div>
+                                : (<video controls>
+                                    <source src="./assets/sampleVideo.mp4" type="video/mp4" />
+                                    <source src="./assets/sampleVideo.ogg" type="video/ogg" />
+                                    Your browser does not support the video tag.
+                                </video>)}
+                        </div>
                     </div>}
+                    <br />
+                    <br />
                     {!guideLinesStateFlag ? <div className="sampleImageDiv">
                         <img src='./assets/sampleImage.jpg' />
                     </div> : null}
                     {guideLinesStateFlag ? <button className="continueBtn" onClick={(e) => changePageTwo(e)}>Continue</button> : null}
-                   <br/>
+                    <br />
+                    <br />
                     <div className="footerDiv">
                         <a className="footerAnchor" href="mailto:abc@example.com?subject = Feedback&body = Message">Email Your Manager For Additional Questions</a>
                     </div>
